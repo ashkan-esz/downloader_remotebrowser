b3bf130c8394f107712ffdcd545b81b03d564955
ashkan <ashkanaz2828@gmail.com>
2022-09-24 23:55:01 +0330

    Remove: tesseract.js to reduce project size

--- captchaSolver.js
+++ captchaSolver.js
@@ -2,7 +2,7 @@ import config from "./config/index.js";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 import FormData from "form-data";
-import {createWorker} from "tesseract.js";
+// import {createWorker} from "tesseract.js";
 import {saveError} from "./saveError.js";
 
 axiosRetry(axios, {
@@ -42,26 +42,26 @@ export async function getCaptchaCode(captchaImage) {
         saveError(error);
     }
 
-    try {
-        if (!captchaCode) {
-            tesseractCounter++;
-            while (tesseractCounter > 1) {
-                await new Promise(resolve => setTimeout(resolve, 100));
-            }
-            let imageBuffer = Buffer.from(captchaImage, "base64");
-            const worker = createWorker();
-            await worker.load();
-            await worker.loadLanguage('eng');
-            await worker.initialize('eng');
-            const {data: {text}} = await worker.recognize(imageBuffer);
-            await worker.terminate();
-            captchaCode = text;
-            tesseractCounter--;
-        }
-    } catch (error) {
-        saveError();
-        tesseractCounter--;
-    }
+    // try {
+    //     if (!captchaCode) {
+    //         tesseractCounter++;
+    //         while (tesseractCounter > 1) {
+    //             await new Promise(resolve => setTimeout(resolve, 100));
+    //         }
+    //         let imageBuffer = Buffer.from(captchaImage, "base64");
+    //         const worker = createWorker();
+    //         await worker.load();
+    //         await worker.loadLanguage('eng');
+    //         await worker.initialize('eng');
+    //         const {data: {text}} = await worker.recognize(imageBuffer);
+    //         await worker.terminate();
+    //         captchaCode = text;
+    //         tesseractCounter--;
+    //     }
+    // } catch (error) {
+    //     saveError();
+    //     tesseractCounter--;
+    // }
 
     return captchaCode;
 }
