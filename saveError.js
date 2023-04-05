464853e281daea48839b84fe62ed1a0ebcd873c4
ashkan <ashkanaz2828@gmail.com>
2023-04-05 22:24:29 +0330

    Feature: better save error function

--- saveError.js
+++ saveError.js
@@ -3,7 +3,10 @@ import * as Sentry from "@sentry/node";
 
 export async function saveError(error, moreInfo = false) {
     if (config.nodeEnv === 'production') {
-        if (moreInfo) {
+        if (!error.url && error.config?.url) {
+            error.url = error.config.url;
+        }
+        if (moreInfo || error.isAxiosError || error.name === "AxiosError") {
             Sentry.withScope(function (scope) {
                 scope.setExtra('ErrorData', error);
                 scope.setTag("ErrorData", "ErrorData");
